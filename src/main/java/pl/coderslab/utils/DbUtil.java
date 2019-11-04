02b87b2e3806dafcdbe619cd2decf1687e0a0fbc
yarikwest <y.zakhidnyi@gmail.com>
2019-11-04 17:38:12 +0100

    Prepare project to deploy

--- src/main/java/pl/coderslab/utils/DbUtil.java
+++ src/main/java/pl/coderslab/utils/DbUtil.java
@@ -1,28 +1,17 @@
 package pl.coderslab.utils;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-    private static DataSource ds;
+    private static DataSource dataSource;
 
-    public static Connection getConn() throws SQLException {
-        return getInstance().getConnection();
-    }
-
-    private static DataSource getInstance() {
-        if (ds == null) {
-            try {
-                Context ctx = new InitialContext();
-                ds = (DataSource) ctx.lookup("java:comp/env/jdbc/crm");
-            } catch (NamingException e) {
-                e.printStackTrace();
-            }
-        }
-        return ds;
+    public static Connection getConnection() throws SQLException {
+        String url = System.getenv("CLEARDB_DATABASE_URL");
+        String user = System.getenv("CLEARDB_DATABASE_USER");
+        String pass = System.getenv("CLEARDB_DATABASE_PASS");
+        return DriverManager.getConnection(url, user, pass);
     }
 }
